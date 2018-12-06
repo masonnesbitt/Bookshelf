811403d6e68e4aeff8fb01829732310bc3de7fc3
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-12-06 10:08:55 -0600

    Adding Segues

---
+++ BookShelf/customCell.swift
@@ -0,0 +1,21 @@
+//
+//  customCell.swift
+//  BookShelf
+//
+//  Created by Mason Nesbitt on 11/29/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import UIKit
+
+
+
+class customTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var bioLabel: UILabel!
+
+    
+    
+}
+
+
